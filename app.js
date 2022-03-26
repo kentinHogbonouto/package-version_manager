566b4dc4cfda1e2fdf451acc36d938f8aa589b28
kentin HOGBONOUTO <kentinhogbonouto1@gmail.com>
2022-03-26 14:29:42 +0100

    update

---
+++ app.js
@@ -0,0 +1,60 @@
+const CronJob = require("cron").CronJob;
+const { exec } = require("child_process");
+const fs = require("fs");
+const sgMail = require("@sendgrid/mail");
+
+job = new CronJob(
+  "*/60 * * * * *",
+  () => {
+    exec("npm outdated", (err, stdout, stderr) => {
+      if (err) {
+        console.error(err.message);
+      }
+      if (stderr) {
+        console.log(stderr);
+      }
+      const outPut = JSON.stringify(stdout);
+      console.log(`stdout: ${outPut}`);
+      const redirectOutput = fs.WriteStream("./update-package.txt");
+      process.stdout.write = process.stderr.write =
+        redirectOutput.write.bind(redirectOutput);
+
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+      const msg = {
+        to: "test@example.com", // Change to your recipient
+        from: "test@example.com", // Change to your verified sender
+        subject: "Sending with SendGrid is Fun",
+        text: "and easy to do anywhere, even with Node.js",
+        html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+      };
+
+      sgMail
+        .send(msg)
+        .then((response) => {
+          console.log(response[0].statusCode);
+          console.log(response[0].headers);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+  },
+  null,
+  true,
+  "America/Los_Angeles"
+);
+
+// child.stdout.on("data", (data) => {
+//   console.log("Stdout:\n", Buffer.from(data).toString());
+// });
+// child.stderr.on("data", (data) => {
+//   console.error(Buffer.from(data).toString());
+// });
+// child.on("error", (error) => {
+//   console.error("Error:\n", error);
+// });
+// child.on("exit", (code, signal) => {
+//   if (code) console.error("Process exit with code:", code);
+//   else if (signal) console.error("Process killed with signal:", signal);
+//   else console.log("Done successfull ✅");
+// });
--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const CronJob = require("cron").CronJob;
-const { exec} = require("child_process");
-const fs = require('fs');
+const { spawn } = require("child_process");
+const fs = require("fs");
 
 const job = new CronJob(
   "*/15 * * * * *",
   () => {
-    const ls = exec("ncu --jsonAll --packageFile package.json", (err, stdout, stderr) =>{
-      if(err){
+    spawn("ls",  (err, stdout, stderr) => {
+      if (err) {
         const error = new Error(err.message);
         error.status = 404;
         throw error;
       }
-      if(stderr){
+      if (stderr) {
         console.log(`stderr: ${stderr}`);
       }
       // console.log(`stdout: ${stdout}`);
-      const outPut = JSON.parse(stdout);
+      // const outPut = JSON.parse(stdout);
       console.log(`outPut: ${stdout}`);
-      // const redirectOutput = fs.WriteStream('./package-updates.json');
+      // const redirectOutput = fs.WriteStream('./update-package.txt');
       // process.stdout.write = process.stderr.write = redirectOutput.write.bind(redirectOutput);
     });
   },
