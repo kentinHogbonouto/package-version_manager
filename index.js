da8840e938324e63bf571443f4d57260e2b62193
kentin HOGBONOUTO <kentinhogbonouto1@gmail.com>
2022-03-28 19:43:53 +0200

    updated index file

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const Job = new CronJob(
       return new Promise((resolve, reject) => {
         exec(command, (error, stdout, stderr) => {
           if (error !== "") {
-            console.log(error.message);
+            console.log(error);
           }
           if (stderr !== "") {
             console.log(`stderr:  ${stderr}`);
             reject(stderr);
           } else {
             resolve(stdout);
-            console.log("stdout" + stdout);
+            console.log(`stdout:  ${stdout}`);
           }
         });
       });
